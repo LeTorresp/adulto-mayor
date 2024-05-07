8436f2bed74ab585a8e0ceb59716658f41bd7ff0
luistope98 <33384695+luistope98@users.noreply.github.com>
2024-05-07 12:08:52 -0500

    ajustes home

--- js/carruseles-home.js
+++ js/carruseles-home.js
@@ -3,95 +3,112 @@
 
 
 
-    var swiper = new Swiper(".Banner-slide", {
-        pagination: {
+var swiper = new Swiper(".Banner-slide", {
+  pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-            clickable: true,
-        },
-        slidesPerView: 1,
-        autoplay: true,
-        freeMode: true,
-        speed: 1000,
-        rewind: true,
-        effect: 'fade',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    clickable: true,
+  },
+  slidesPerView: 1,
+  autoplay: true,
+  freeMode: true,
+  speed: 1000,
+  rewind: true,
+  effect: 'fade',
 
-    });
-
-
+});
 
 
 
-    document.getElementById('cambiarASlide1').addEventListener('click', function () {
-        swiper.slideTo(0);
-        console.log("slide1");
-    });
+$("#cambiarASlide1").click(function(){
+  swiper.slideTo(0);
 
+});
 
-    document.getElementById('cambiarASlide2').addEventListener('click', function () {
-        swiper.slideTo(1);
-        console.log("texto2");
-    });
+$("#cambiarASlide2").click(function(){
+  swiper.slideTo(1);
+ 
 
-    document.getElementById('cambiarASlide3').addEventListener('click', function () {
-        console.log("texto3");
-        swiper.slideTo(2);
-       
-    });
+});
 
+$("#cambiarASlide3").click(function(){
+  swiper.slideTo(2);
 
+});
 
-    var swiper2 = new Swiper(".Interes", {
+var swiper2 = new Swiper(".Interes", {
   pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
+    el: ".swiper-pagination",
+    dynamicBullets: true,
   },
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-  
+
   slidesPerView: 3,
   spaceBetween: 20,
-    autoplay: true,
-    freeMode: true,
-    speed: 1000,
-    rewind: true,
-    breakpoints: {
-      900: {
-        slidesPerView: 4,
-        spaceBetween: 50,
-      },
+  autoplay: true,
+  freeMode: true,
+  speed: 1000,
+  rewind: true,
+  breakpoints: {
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 50,
     },
+  },
+});
+
+var swiper2 = new Swiper(".portafolio-slide", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 4,
+  autoplay: true,
+  freeMode: true,
+  speed: 1000,
+  rewind: true,
+  breakpoints: {
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
 
 
 
 var swiper3 = new Swiper(".Sedes", {
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 3,
+  spaceBetween: 20,
+  autoplay: true,
+  freeMode: true,
+  speed: 1000,
+  rewind: true,
+  breakpoints: {
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 50,
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    
-    slidesPerView: 3,
-    spaceBetween: 20,
-      autoplay: true,
-      freeMode: true,
-      speed: 1000,
-      rewind: true,
-      breakpoints: {
-        900: {
-          slidesPerView: 3,
-          spaceBetween: 50,
-        },
-      },
-  });
-  
+  },
+});
